c1f36f1b0aa69a1a13a4fffcfdc3a9856b266eae
pedrok30k1 <130935765+PedroIndex@users.noreply.github.com>
2023-10-25 22:24:54 -0300

    trabalho

--- quiz do trabalho/quiz.js
+++ quiz do trabalho/quiz.js
@@ -1,10 +1,11 @@
-var tempoLimite = 20; // Tempo limite de resposta em segundos
+var tempoLimite = 30; // Tempo limite de resposta em segundos
 var contador = tempoLimite;
 var temporizador;
 var respostas = []; // Array para armazenar as respostas do usuário
+var pontos = 0; // Pontuação inicial
+var pontosPerdidos = -2; // Pontos perdidos quando o tempo acaba
+
 var perguntas = [
-
-
     {
         pergunta: 'Qual time é do Rio de Janeiro?',
         opcoes: ['a) São Paulo', 'b) Palmeiras', 'c) Santos', 'd) Flamengo'],
@@ -65,30 +66,12 @@ var perguntas = [
         imagem: 'pergunta10.jpeg',
         respostaCorreta: 'C'
     }
-
+    
 ];
 
 var perguntaAtual = 0;
 
-function responder() {
-    clearInterval(temporizador); // Limpa o temporizador atual
-    var resposta = document.querySelector('input[name="opcao"]:checked');
-    if (resposta) {
-        respostas.push(resposta.value);
-        var respostaCorreta = perguntas[perguntaAtual].respostaCorreta;
-        var respostaUsuario = resposta.value;
-        var respostaElement = document.getElementById('resposta');
-        if (respostaUsuario === respostaCorreta) {
-            respostaElement.classList.add('resposta-correta');
-            respostaElement.innerHTML = 'Parabéns, você acertou!';
-        } else {
-            respostaElement.classList.add('resposta-incorreta');
-            respostaElement.innerHTML = 'Que pena, você errou. A resposta: ' + respostaCorreta + '.';
-        }
-        document.getElementById('quiz-container').style.display = 'none';
-        document.getElementById('resposta-container').style.display = 'block';
-    } 
-}
+
 
 function proximaPergunta() {
     perguntaAtual++;
@@ -124,10 +107,11 @@ function proximaPergunta() {
     }
 }
 
+
 function contarTempo() {
     if (contador === 0) {
         clearInterval(temporizador);
-       
+        pontos += pontosPerdidos; // Subtrai 2 pontos quando o tempo acaba
         proximaPergunta();
     } else {
         document.getElementById('tempo').innerHTML = contador + 's';
@@ -135,6 +119,69 @@ function contarTempo() {
     }
 }
 
+
+function responder() {
+    clearInterval(temporizador); // Limpa o temporizador atual
+    var resposta = document.querySelector('input[name="opcao"]:checked');
+    if (resposta) {
+        respostas.push(resposta.value);
+        var respostaCorreta = perguntas[perguntaAtual].respostaCorreta;
+        var respostaUsuario = resposta.value;
+        var respostaElement = document.getElementById('resposta');
+        if (respostaUsuario === respostaCorreta) {
+            respostaElement.classList.add('resposta-correta');
+            respostaElement.innerHTML = 'Parabéns, você acertou!';
+            pontos += 2; // Adiciona 2 pontos para cada pergunta acertada
+        } else {
+            respostaElement.classList.add('resposta-incorreta');
+            respostaElement.innerHTML = 'Que pena, você errou. A resposta correta é: ' + respostaCorreta + '.';
+            pontos += -1; // Não adiciona pontos para perguntas erradas
+        }
+        document.getElementById('quiz-container').style.display = 'none';
+        document.getElementById('resposta-container').style.display = 'block';
+        document.getElementById('pontos').innerHTML = 'Pontuação: ' + pontos; // Atualiza a exibição dos pontos
+    }
+}
+
+
+
+function proximaPergunta() {
+    perguntaAtual++;
+    if (perguntaAtual < perguntas.length) {
+        document.getElementById('quizForm').reset();
+        document.getElementById('resposta').classList.remove('resposta-correta', 'resposta-incorreta');
+        document.getElementById('quiz-container').style.display = 'block';
+        document.getElementById('resposta-container').style.display = 'none';
+        document.querySelector('h1').innerHTML = 'Pergunta ' + (perguntaAtual + 1) + ':';
+        document.querySelector('img').src = 'imagem/' + perguntas[perguntaAtual].imagem;
+        document.querySelector('p').innerHTML = perguntas[perguntaAtual].pergunta;
+        document.getElementById('pontos').innerHTML = 'Pontuação: ' + pontos;
+        document.querySelector('input[value="A"]').nextSibling.nodeValue = perguntas[perguntaAtual].opcoes[0];
+        document.querySelector('input[value="B"]').nextSibling.nodeValue = perguntas[perguntaAtual].opcoes[1];
+        document.querySelector('input[value="C"]').nextSibling.nodeValue = perguntas[perguntaAtual].opcoes[2];
+        document.querySelector('input[value="D"]').nextSibling.nodeValue = perguntas[perguntaAtual].opcoes[3];
+        contador = tempoLimite;
+        document.getElementById('tempo').innerHTML = contador + 's';
+        temporizador = setInterval(contarTempo, 1000);
+    } else {
+        var numRespostasCorretas = 0;
+        for (var i = 0; i < perguntas.length; i++) {
+            if (respostas[i] === perguntas[i].respostaCorreta) {
+                numRespostasCorretas++;
+            }
+        }
+        var resultado = 'Você acertou ' + numRespostasCorretas + ' de ' + perguntas.length + ' perguntas.';
+        document.querySelector('h1').innerHTML = 'Resultado:';
+        document.querySelector('p').innerHTML = resultado;
+        document.getElementById('quiz-container').style.display = 'none';
+        document.getElementById('resposta-container').style.display = 'none';
+        document.getElementById('resultado-container').style.display = 'block';
+        document.getElementById('resultado').innerHTML = resultado;
+    }
+}
+
+
+document.getElementById('pontos').innerHTML = 'Pontuação: ' + pontos;
 document.querySelector('p').innerHTML = perguntas[perguntaAtual].pergunta;
 document.querySelector('img').src = 'imagem/' + perguntas[perguntaAtual].imagem;
 document.querySelector('input[value="A"]').nextSibling.nodeValue = perguntas[perguntaAtual].opcoes[0];
